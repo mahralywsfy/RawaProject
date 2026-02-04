4c5d14d9cf8df13172d83f886eb9c0592a200c6b
mahralywsfy <mahralywsfy59@gmail.com>
2026-02-04 17:48:51 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Rawa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(267, 27);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(134, 24);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(267, 161);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(134, 24);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(267, 275);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(134, 24);
+            this.txtPass.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(438, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(675, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "submit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Rawa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. التأكد أولاً أن الحقول ليست فارغة قبل الحفظ
+                if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtEmail.Text) || string.IsNullOrWhiteSpace(txtPass.Text))
+    {
+                    MessageBox.Show("تنبيه: لا يمكن حفظ بيانات فارغة! يرجى تعبئة كافة الخانات.", "خطأ في الحفظ", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+    else
+                {
+                    // 2. إذا كانت الحقول ممتلئة، يتم الحفظ
+                    string filePath = "data.txt";
+                    string info = "Name: " + txtName.Text + " | Email: " + txtEmail.Text + " | Password: " + txtPass.Text + Environment.NewLine;
+
+                    File.AppendAllText(filePath, info);
+
+                    MessageBox.Show("تم حفظ البيانات بنجاح في الملف!", "نجاح الحفظ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("حدث خطأ غير متوقع: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 1. التحقق من أن الخانات ليست فارغة
+            if (string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtEmail.Text) || string.IsNullOrWhiteSpace(txtPass.Text))
+    {
+                MessageBox.Show("عذراً، يجب تعبئة كافة الخانات!", "تنبيه");
+            }
+    // 2. التحقق من صحة الإيميل (يجب أن يحتوي على @ و نقطة معاً)
+    // لاحظي استخدمنا && التي تعني "و" مع علامة ! لضمان وجود الرمزين
+            else if (!txtEmail.Text.Contains("@") || !txtEmail.Text.Contains("."))
+            {
+                MessageBox.Show("صيغة الإيميل غير صحيحة! تأكدي من وجود @ ونقطة .", "خطأ في الإدخال");
+            }
+            // 3. التحقق من طول كلمة المرور
+            else if (txtPass.Text.Length < 6)
+            {
+                MessageBox.Show("كلمة المرور يجب أن تكون 6 خانات أو أكثر", "تنبيه");
+            }
+            else
+            {
+                MessageBox.Show("تم التحقق والإرسال بنجاح!", "نجاح");
+                // اختياري: مسح الخانات بعد النجاح
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtEmail.Clear();
+            txtPass.Clear();
+            txtName.Focus(); // يضع الماوس على أول خانة تلقائياً
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("data.txt"))
+            {
+                // قراءة كل الأسطر من الملف
+                string[] lines = File.ReadAllLines("data.txt");
+
+                // إذا كان الملف غير فارغ، اعرض آخر سطر تم حفظه
+                if (lines.Length > 0)
+                {
+                    string lastLine = lines[lines.Length - 1];
+                    MessageBox.Show("آخر بيانات محفوظة هي: \n" + lastLine);
+                }
+            }
+            else
+            {
+                MessageBox.Show("لا يوجد ملف بيانات بعد!");
+            }
+        }
+    }
+}
